b7e363518425babacc0fe03ccc093ba0bff016b0
jekokiteishvili <jemali.kiteishvili@gmail.com>
2021-12-11 00:49:06 +0400

    js

--- js/first.js
+++ js/first.js
@@ -1,13 +1,32 @@
-var myname = "jeko";
+// let mycolor = "green";
 
-let mycolor = "green";
+// document.getElementById("output").innerHTML = "<h1>" + mycolor; + "</h1>";
 
-const lastname = "kiteishvili";
-
-console.log(myname)
-
-console.log(mycolor)
-
-console.log(lastname)
+// console.log(mycolor)
 
 
+
+let undfinde = undefined;
+
+let numb = 55; 
+
+let string = "I am string";
+
+let array = [1, 2, 3, "jeko"];
+
+let object = {
+    saxeli: "jeko",
+    gvari: "kiteishvili",
+   
+};
+
+let bool = true;
+let bool2 = false;
+
+let funct = function () {};
+
+console.log(10 == 10)
+
+console.log(typeof(bool));
+
+document.getElementById("text").innerHTML ="<h1>" + string; + "</h1>"
