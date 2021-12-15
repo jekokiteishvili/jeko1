c5564ad47177347f3bec034aff797a7eed642e53
jekokiteishvili <jemali.kiteishvili@gmail.com>
2021-12-15 19:22:14 +0400

    es

--- js/string.js
+++ js/string.js
@@ -1,5 +1,5 @@
 let url = "https://jekokiteishvili.github.io/jeko1/ofice.html";
-let img = "https://pixy.org/src/20/201310.jpg"
+let img = "../img/R.jpg"
 let text = "Some quick example text to build on the card title and make up the bulk of the card's content."
 let myname = `<div class = "col-lg-4" <div class="card">
 <img src="${img}" class="card-img-top" alt="...">
