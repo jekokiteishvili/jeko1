9a3730661af063e8ca4bcb159feaadd5174700e6
jekokiteishvili <jemali.kiteishvili@gmail.com>
2021-12-15 16:20:31 +0400

    string

--- js/first.js
+++ js/first.js
@@ -29,4 +29,31 @@ console.log(10 == 10)
 
 console.log(typeof(bool));
 
+
+
 document.getElementById("text").innerHTML ="<h1>" + string; + "</h1>"
+
+
+
+//stringebi
+
+let mystring = "მე ვსწავლობ front-end web პროგრამირებას"
+
+console.log(mystring.length) // სიმბოლოების რაოდენობა
+console.log(mystring.slice(2, 11))  //ამოჭრა
+console.log(mystring.search("front-end")) //ეძებს სტრინგში არსებულ სიტყვა/სიმბოლოებს
+console.log(mystring.includes("web")) //სტრინგში არის თუ არა ეს სიტყვა/სიმბოლო
+console.log(mystring.startsWith("web")) //სტრინგი იწყება თუ არა ამ სიტყვით
+console.log(mystring.replace("front-end","back-end")) //ჩანაცვლება პირველი სიტყვების/სიმბოლოების
+console.log(mystring.replaceAll("front-end","back-end")) //ჩანაცვლება ყველა სიტყვების/სიმბოლოების
+console.log(mystring.toUpperCase()) //სტრინგი დიდი ასოებით 
+console.log(mystring.trim().length) //შლის სიცარიელეებს დასაწყისში და დასასრულში 
+//სტრინგისგან ვქმნით მასივს
+let myarray = mystring.split(" ");
+console.log(myarray) 
+
+//ცვლადის გატანება ტემპლეიტ სინტაქსით
+let name = "ჯეკო";
+let myname = `მე მქვია ${name}`
+document.getElementById("string").innerHTML = myname
+
---
+++ js/string.js
@@ -0,0 +1,35 @@
+let url = "https://jekokiteishvili.github.io/jeko1/ofice.html";
+let img = "../img/R.jpg"
+let text = "Some quick example text to build on the card title and make up the bulk of the card's content."
+let myname = `<div class = "col-lg-4" <div class="card">
+<img src="${img}" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">${text}</p>
+  <a href="${url}" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</div>`
+document.getElementById("text").innerHTML = myname
+
+
+let mystring = "მე ვსწავლობ front-end web პროგრამირებას"
+
+console.log(mystring.slice(12, -18));
+
+console.log(mystring.search("front-end"));
+
+console.log(mystring.includes("web"));
+
+console.log(mystring.startsWith("web"));
+
+console.log(mystring.replace("front-end","back-end"));
+
+console.log(mystring.toUpperCase());
+
+console.log(mystring.trim().length);
+
+let array = mystring.split(" ");
+console.log(array);
+
+
